ffae96456964864593ab032b03597e00c0a50641
David Vergaray <david.vergaray.a@gmail.com>
2022-01-03 17:41:31 -0300

    Add Book Function Complete

--- script.js
+++ script.js
@@ -3,9 +3,44 @@ function Book(name, author) {
   this.author = author;
 }
 
+window.onload = function() {
+  setLocalStorage();
+}
+
+function setLocalStorage() {
+  const localObj = localStorage.getItem('bookArray');
+  const bookContainer = document.getElementById('bookContainer');
+  for (let i = 0; i < JSON.parse(localObj).length; i++) {
+    const bookDiv = document.createElement('div');
+    bookDiv.classList.add('book');
+    bookDiv.innerHTML = `
+    <p>${JSON.parse(localObj)[i].name}</p>
+    <p>${JSON.parse(localObj)[i].author}</p>
+    <button>Remove</button>
+    <hr size="2" width="100%" color="gray">
+    `;
+    bookContainer.appendChild(bookDiv);
+  }
+}
+
 function addBook(book) {
   bookArray.push(book);
-  console.log(bookArray);
+  localStorage.setItem('bookArray', JSON.stringify(bookArray));
+
+  const bookContainer = document.getElementById('bookContainer');
+
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+  bookDiv.innerHTML = `
+  <p>${book.name}</p>
+  <p>${book.author}</p>
+  <button>Remove</button>
+  <hr size="2" width="100%" color="gray">
+  `;
+
+  bookContainer.appendChild(bookDiv);
+
+  return bookArray;
 }
 
 function createBook() {
@@ -15,5 +50,10 @@ function createBook() {
   return new Book(name, author);
 }
 
-let bookArray = [];
+function createHTMLBook(params) {
+  
+}
 
+let bookArray = JSON.parse(localStorage.getItem('bookArray'));
+
+console.log(bookArray);
