5d271cd382a9e5284b5c56180201c995b6423278
David Vergaray <david.vergaray.a@gmail.com>
2022-01-03 18:46:22 -0300

    Beggining Remove Function

--- script.js
+++ script.js
@@ -3,19 +3,37 @@ function Book(name, author) {
   this.author = author;
 }
 
+function createHTMLBook(bName, bAuthor) {
+  const bookDiv = document.createElement('div');
+  bookDiv.classList.add('book');
+
+  const name = document.createElement('p');
+  name.textContent = bName;
+  bookDiv.appendChild(name);
+
+  const author = document.createElement('p');
+  author.textContent = bAuthor;
+  bookDiv.appendChild(author);
+
+  const remButt = document.createElement('button');
+  remButt.textContent = 'Remove';
+  remButt.classList.add('remButton');
+  bookDiv.appendChild(remButt);
+
+  const line = document.createElement('hr');
+  line.setAttribute('size','2');
+  line.setAttribute('width','100%');
+  line.setAttribute('color', 'gray');
+  bookDiv.appendChild(line);
+
+  return bookDiv;
+}
+
 function setLocalStorage() {
   const localObj = localStorage.getItem('bookArray');
   const bookContainer = document.getElementById('bookContainer');
   for (let i = 0; i < JSON.parse(localObj).length; i++) {
-    const bookDiv = document.createElement('div');
-    bookDiv.classList.add('book');
-    bookDiv.innerHTML = `
-    <p>${JSON.parse(localObj)[i].name}</p>
-    <p>${JSON.parse(localObj)[i].author}</p>
-    <button>Remove</button>
-    <hr size="2" width="100%" color="gray">
-    `;
-    bookContainer.appendChild(bookDiv);
+    bookContainer.appendChild(createHTMLBook(JSON.parse(localObj)[i].name, JSON.parse(localObj)[i].author));
   }
 }
 
@@ -24,17 +42,7 @@ function addBook(book) {
   localStorage.setItem('bookArray', JSON.stringify(bookArray));
 
   const bookContainer = document.getElementById('bookContainer');
-
-  const bookDiv = document.createElement('div');
-  bookDiv.classList.add('book');
-  bookDiv.innerHTML = `
-  <p>${book.name}</p>
-  <p>${book.author}</p>
-  <button>Remove</button>
-  <hr size="2" width="100%" color="gray">
-  `;
-
-  bookContainer.appendChild(bookDiv);
+  bookContainer.appendChild(createHTMLBook(book.name, book.author));
 
   return bookArray;
 }
@@ -51,3 +59,13 @@ window.onload = function() {
 }
 
 let bookArray = JSON.parse(localStorage.getItem('bookArray'));
+
+
+console.log("RemoveButtons:", document.querySelectorAll('.remButton'));
+
+let remButtons = document.querySelectorAll('.remButton');
+
+// remButtons.forEach((button) => {
+//   button.addEventListener('click', button.parentElement.remove());
+// });
+
