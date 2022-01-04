dfd5af5799a05c5660906a228b8979b740ab17f1
Aduda-Boaz <adudaboaz@gmail.com>
2022-01-05 01:56:31 +0300

    Modify the Css

--- script.js
+++ script.js
@@ -2,35 +2,27 @@ function Book(name, author) {
   this.name = name;
   this.author = author;
 }
-
 class Books {
   constructor() {
     this.storage = [];
   }
-
   add(book) {
     this.storage.push(book);
   }
-
   remove(index) {
     this.storage.splice(index, 1);
   }
-
   set setBooks(books) {
     this.storage = books;
   }
 }
-
 function saveBooksInLocalStorage(books) {
   localStorage.setItem('bookArray', JSON.stringify(books));
 }
-
 function getBooksFromLocalStorage() {
   return JSON.parse(localStorage.getItem('bookArray'));
 }
-
 const bookStor = new Books();
-
 function removeBook(remButt) {
   const books = remButt.parentElement.parentElement.children;
   for (let i = 0; i < books.length; i += 1) {
@@ -41,38 +33,23 @@ function removeBook(remButt) {
     }
   }
 }
-
 function createHTMLBook(bName, bAuthor) {
   const bookDiv = document.createElement('div');
-  bookDiv.classList.add('book');
-
+  bookDiv.classList.add('book', 'd-flex', 'bg-gray', 'justify-content-between', 'p-2');
   const name = document.createElement('p');
-  name.textContent = bName;
+  name.textContent = `${bName} by ${bAuthor}`;
+  name.classList.add('align-self-center', 'my-1');
   bookDiv.appendChild(name);
-
-  const author = document.createElement('p');
-  author.textContent = bAuthor;
-  bookDiv.appendChild(author);
-
   const remButt = document.createElement('button');
   remButt.textContent = 'Remove';
-  remButt.classList.add('remButton');
+  remButt.classList.add('remButton', 'border', 'border-2', 'border-dark');
   bookDiv.appendChild(remButt);
-
   remButt.addEventListener('click', () => {
     removeBook(remButt);
     remButt.parentElement.remove();
   });
-
-  const line = document.createElement('hr');
-  line.setAttribute('size', '2');
-  line.setAttribute('width', '100%');
-  line.setAttribute('color', 'gray');
-  bookDiv.appendChild(line);
-
   return bookDiv;
 }
-
 function setLocalStorage() {
   const localObj = localStorage.getItem('bookArray');
   const bookContainer = document.getElementById('bookContainer');
@@ -82,31 +59,27 @@ function setLocalStorage() {
     bookContainer.appendChild(createHTMLBook(name, author));
   }
 }
-
 function addBook(newBook) {
   bookStor.add(newBook);
-
   saveBooksInLocalStorage(bookStor.storage);
-
   const bookContainer = document.getElementById('bookContainer');
   bookContainer.appendChild(createHTMLBook(newBook.name, newBook.author));
 }
-
 const addBookbtn = document.getElementById('addBookbtn');
 addBookbtn.onclick = function addABook() {
   const name = document.getElementById('addName').value;
   const author = document.getElementById('addAuthor').value;
-
-  const newBook = new Book(name, author);
-
-  addBook(newBook);
+  if (name !== '' && author !== '') {
+    const newBook = new Book(name, author);
+    addBook(newBook);
+    document.getElementById('addName').value = null;
+    document.getElementById('addAuthor').value = null;
+  }
 };
-
 const localObj = localStorage.getItem('bookArray');
 if (localObj != null) {
   setLocalStorage();
 }
-
 if (localStorage.getItem('bookArray') != null) {
   bookStor.setBooks = getBooksFromLocalStorage();
 }
